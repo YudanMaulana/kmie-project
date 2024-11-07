db538206576ff5a8cfc43dbb322aee52bed1100b
YudanMaulana <yudanmaulana21@gmail.com>
2024-11-07 22:52:48 +0700

    repair splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,26 +1,66 @@
-// splash_screen.dart
-
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:kme_project_test/login.dart';
-import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return FlutterSplashScreen.gif(
-      gifPath: 'assets/furina.gif',
-      gifWidth: 269,
-      gifHeight: 474,
-      nextScreen: const LoginPage(),
-      duration: const Duration(milliseconds: 3500),
-      onInit: () async {
-        debugPrint("onInit");
-      },
-      onEnd: () async {
-        debugPrint("onEnd 1");
-      },
+    return Scaffold(
+      body: FlutterSplashScreen(
+        duration: Duration(milliseconds: 3200),
+        splashScreenBody: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 300),
+                  Text(
+                    'KMIE',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFEC4C01),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Knitting Machine Instant Efficiency',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color.fromRGBO(83, 83, 83, 1),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    bottom: 20.0), // Jarak dari bawah layar
+                child: Text(
+                  'by yudan maulana',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Colors.white,
+        nextScreen: LoginPage(),
+      ),
+    );
+  }
+}
+
+class NextScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: Center(child: Text('Welcome to the Home Screen')),
     );
   }
 }

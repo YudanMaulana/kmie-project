2190a4ecff4c87d025bde68278819ee4ecc1f44d
YudanMaulana <yudanmaulana21@gmail.com>
2024-11-15 18:26:58 +0700

    build result table, reset button, converting to minute in result

--- lib/form.dart
+++ lib/form.dart
@@ -1,3 +1,4 @@
+// import 'dart:math';
 import 'package:flutter/material.dart';
 
 class MyForm extends StatefulWidget {
@@ -17,7 +18,10 @@ class _MyFormState extends State<MyForm> {
   double _percentlost = 0;
   double _percentincome = 0;
   double _counterincome = 0;
+  double _tominute = 0;
+
   bool _isFormFilled = false;
+  bool _isResultVisible = false;
 
   @override
   void initState() {
@@ -40,12 +44,12 @@ class _MyFormState extends State<MyForm> {
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
-          title: Text("Perhatian"),
-          content: Text("Semua field harus diisi"),
+          title: const Text("Perhatian"),
+          content: const Text("Semua field harus diisi"),
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context),
-              child: Text("OK"),
+              child: const Text("OK"),
             ),
           ],
         ),
@@ -65,115 +69,96 @@ class _MyFormState extends State<MyForm> {
       _percentlost = double.parse(_percentlost.toStringAsFixed(2));
       _percentincome = ((_countermurni - _lostcounter) / _countermurni) * 100;
       _percentincome = double.parse(_percentincome.toStringAsFixed(4));
+      _tominute = (_countermurni - _lostcounter) / 60;
+      _isResultVisible = true;
     });
   }
 
-  @override
-  void dispose() {
-    _controller1.dispose();
-    _controller2.dispose();
-    _controller3.dispose();
-    super.dispose();
+  void _resetForm() {
+    setState(() {
+      _isResultVisible = false;
+      _countermurni = 0;
+      _lostcounter = 0;
+      _percentlost = 0;
+      _percentincome = 0;
+      _counterincome = 0;
+      _tominute = 0;
+      _controller1.clear();
+      _controller2.clear();
+      _controller3.clear();
+      _isFormFilled = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text.rich(TextSpan(children: [
-          TextSpan(
-              text: "Knitting Machine ",
-              style: TextStyle(
-                  color: Color.fromRGBO(83, 83, 83, 1), fontSize: 22)),
-          TextSpan(
-              text: "Instant Efficiency ",
-              style: TextStyle(color: Color(0xFFEC4C01), fontSize: 22)),
-        ])),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(0.8, 0, 0.8, 0),
-        child: Form(
-          child: Column(
-            children: [
-              _buildTextField('RPM Mesin', _controller1),
-              _buildTextField('Jam Kerja', _controller2),
-              const SizedBox(height: 10),
-              _buildTextField('Total counter operator', _controller3),
-              const SizedBox(height: 20),
-              Text.rich(TextSpan(
-                children: [
-                  const TextSpan(
-                      text: 'total murni ',
-                      style: TextStyle(
-                          fontSize: 20, color: Color.fromRGBO(83, 83, 83, 1))),
-                  TextSpan(
-                      text: '${_countermurni.toInt()}',
-                      style: const TextStyle(
-                          fontSize: 20, color: Color(0xFFEC4C01))),
-                  const TextSpan(
-                      text: ' counter',
-                      style: TextStyle(
-                          fontSize: 20, color: Color.fromRGBO(83, 83, 83, 1))),
-                ],
-              )),
-              const SizedBox(height: 20),
-              Text.rich(TextSpan(
-                children: [
-                  TextSpan(
-                      text: 'pendapatan overall ',
-                      style: TextStyle(
-                          fontSize: 20, color: Color.fromRGBO(83, 83, 83, 1))),
-                  TextSpan(
-                      text: '${_counterincome.toInt()}',
-                      style: TextStyle(fontSize: 20, color: Color(0xFFEC4C01))),
-                  TextSpan(
-                      text: ' atau ',
-                      style: TextStyle(
-                          fontSize: 20, color: Color.fromRGBO(83, 83, 83, 1))),
-                  TextSpan(
-                      text: '${_percentincome.toInt()}%',
-                      style: const TextStyle(
-                          fontSize: 20, color: Color(0xFFEC4C01))),
-                ],
-              )),
-              Text.rich(TextSpan(
-                children: [
-                  const TextSpan(
-                      text: 'Lost ',
-                      style: TextStyle(
-                          fontSize: 20, color: Color.fromRGBO(83, 83, 83, 1))),
-                  TextSpan(
-                      text: '${_lostcounter.toInt()}',
-                      style: const TextStyle(
-                          fontSize: 20, color: Color(0xFFEC4C01))),
-                  const TextSpan(
-                      text: ' counter',
-                      style: TextStyle(
-                          fontSize: 20, color: Color.fromRGBO(83, 83, 83, 1))),
-                  const TextSpan(
-                      text: ' atau ',
-                      style: TextStyle(
-                          fontSize: 20, color: Color.fromRGBO(83, 83, 83, 1))),
-                  TextSpan(
-                      text: '${_percentlost}%',
-                      style: const TextStyle(
-                          fontSize: 20, color: Color(0xFFEC4C01))),
-                ],
-              )),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _isFormFilled ? _calculateLostCounter : null,
-                child: const Text(
-                  "Hitung Hasil Akhir",
-                  style: TextStyle(color: Color(0xFFEC4C01)),
+        appBar: AppBar(
+          title: const Text.rich(TextSpan(children: [
+            TextSpan(
+                text: "Knitting Machine ",
+                style: TextStyle(
+                    color: Color.fromRGBO(83, 83, 83, 1), fontSize: 22)),
+            TextSpan(
+                text: "Instant Efficiency ",
+                style: TextStyle(color: Color(0xFFEC4C01), fontSize: 22)),
+          ])),
+        ),
+        body: SingleChildScrollView(
+          physics: const ClampingScrollPhysics(),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.of(context).size.height,
+            ),
+            child: IntrinsicHeight(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0.8, 0),
+                child: Form(
+                  child: Column(
+                    children: [
+                      _buildTextField('RPM Mesin', _controller1),
+                      _buildTextField('Jam Kerja', _controller2),
+                      const SizedBox(height: 10),
+                      _buildTextField('Total counter operator', _controller3),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed:
+                                _isFormFilled ? _calculateLostCounter : null,
+                            child: const Text(
+                              "Hitung Hasil Akhir",
+                              style: TextStyle(color: Color(0xFFEC4C01)),
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed: _resetForm,
+                            child: const Text(
+                              "Reset",
+                              style: TextStyle(color: Color(0xFFEC4C01)),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      if (_isResultVisible)
+                        DataTableExample(
+                          countermurni: _countermurni,
+                          counterincome: _counterincome,
+                          percentincome: _percentincome,
+                          lostcounter: _lostcounter,
+                          percentlost: _percentlost,
+                          tominute: _tominute,
+                        ),
+                    ],
+                  ),
                 ),
               ),
-              const SizedBox(height: 20),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 
   Widget _buildTextField(String label, TextEditingController controller) {
@@ -181,7 +166,7 @@ class _MyFormState extends State<MyForm> {
       padding: const EdgeInsets.all(7.0),
       child: TextFormField(
         style: const TextStyle(color: Color(0xFFEC4C01)),
-        keyboardType: TextInputType.numberWithOptions(signed: true),
+        keyboardType: const TextInputType.numberWithOptions(signed: true),
         controller: controller,
         decoration: InputDecoration(
           labelText: label,
@@ -193,6 +178,97 @@ class _MyFormState extends State<MyForm> {
           floatingLabelStyle: const TextStyle(color: Color(0xFFEC4C01)),
         ),
       ),
+    );
+  }
+}
+
+class DataTableExample extends StatelessWidget {
+  final double countermurni;
+  final double counterincome;
+  final double percentincome;
+  final double lostcounter;
+  final double percentlost;
+  final double tominute;
+
+  const DataTableExample({
+    super.key,
+    required this.countermurni,
+    required this.counterincome,
+    required this.percentincome,
+    required this.lostcounter,
+    required this.percentlost,
+    required this.tominute,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DataTable(
+      horizontalMargin: 10,
+      columnSpacing: 40,
+      columns: const <DataColumn>[
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Jenis',
+              style: TextStyle(
+                  fontStyle: FontStyle.italic, color: Color(0xFFEC4C01)),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Counter',
+              style: TextStyle(
+                  fontStyle: FontStyle.italic, color: Color(0xFFEC4C01)),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Persen',
+              style: TextStyle(
+                  fontStyle: FontStyle.italic, color: Color(0xFFEC4C01)),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Menit',
+              style: TextStyle(
+                  fontStyle: FontStyle.italic, color: Color(0xFFEC4C01)),
+            ),
+          ),
+        ),
+      ],
+      rows: <DataRow>[
+        DataRow(
+          cells: <DataCell>[
+            const DataCell(Text('Counter murni')),
+            DataCell(Text('${countermurni.toInt()}')),
+            const DataCell(Text('100%')),
+            DataCell(Text('-')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+            const DataCell(Text('Pendapatan')),
+            DataCell(Text('${counterincome.toInt()}')),
+            DataCell(Text('${percentincome.toInt()}%')),
+            DataCell(Text('-')),
+          ],
+        ),
+        DataRow(
+          cells: <DataCell>[
+            const DataCell(Text('Lost Counter')),
+            DataCell(Text('${lostcounter.toInt()}')),
+            DataCell(Text('${percentlost}%')),
+            DataCell(Text('${tominute.toInt()}')),
+          ],
+        ),
+      ],
     );
   }
 }
